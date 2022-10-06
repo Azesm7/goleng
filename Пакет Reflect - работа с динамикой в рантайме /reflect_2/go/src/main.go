232fcffcf21f05da63b6410d1adfcd226502d026
Roman <113186071+Azesm7@users.noreply.github.com>
2022-10-06 18:01:50 +0700

    Update main.go

--- Пакет Reflect - работа с динамикой в рантайме /reflect_2/go/src/main.go
+++ Пакет Reflect - работа с динамикой в рантайме /reflect_2/go/src/main.go
@@ -8,10 +8,10 @@ import (
 )
 
 type User struct {
-	Id       int
-	UserName string `unpack:"-"`
+	ID       int
+	RealName string `unpack:"-"`
 	Login    string
-	flog     int
+	flags    int
 }
 
 func UnpackReflect(u interface{}, data []byte) error {
@@ -25,15 +25,17 @@ func UnpackReflect(u interface{}, data []byte) error {
 		}
 		switch typeFaild.Type.Kind() { // предстовление типа
 		case reflect.Int: // если содержит тип int
-			var value int32                             // создаём переменую
+			var value uint32                            // создаём переменую
 			binary.Read(r, binary.LittleEndian, &value) // считываем данные из буфера
 			valField.Set(reflect.ValueOf(int(value)))   // устанавливаем зпредставление тех данных которые нам нужны
 		case reflect.String: // если содержит тип string
-			var lenRaw uint32                             // создание переменой
-			binary.Read(r, binary.LittleEndian, &data)    // считываем данные из буфера
-			dataRaw := make([]byte, lenRaw)               //создание слайса байт
-			binary.Read(r, binary.LittleEndian, &dataRaw) // читаю в него данные
-			valField.SetString(string(dataRaw))           //присвоение значение в поле структуры
+			var lenRaw uint32                            // создание переменой
+			binary.Read(r, binary.LittleEndian, &lenRaw) // считываем данные из буфера
+			dataRaw := make([]byte, lenRaw)
+			//создание слайса байт
+			binary.Read(r, binary.LittleEndian, &lenRaw) // читаю в него данные
+			valField.SetString(string(dataRaw))
+			//присвоение значение в поле структуры
 		default:
 			return fmt.Errorf("bad type: %v for faild %v", typeFaild.Type.Kind(), typeFaild.Name)
 		}
@@ -43,11 +45,12 @@ func UnpackReflect(u interface{}, data []byte) error {
 }
 func main() {
 	/*
-		parl -E =pack("L L/a", 1_123_456, "v.romanov", 16);
-		    Print map {ord."," } split{"", $b};
+		parl - E '$b = pack("L L/a* L", 1_123_456, "v.romanov", 16);
+		    print map { ord.", " } split{"", $b}; '
 	*/
 	data := []byte{ //байтовое  пристовление запокованого
 		128, 36, 17, 0,
+
 		9, 0, 0, 0,
 		118, 46, 114, 111, 109, 97, 110, 111, 118,
 
