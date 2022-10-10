46e0e186df35f0baa9d825fe841de3cdaa3cce3d
Roman <113186071+Azesm7@users.noreply.github.com>
2022-10-10 18:21:22 +0700

    Update main.go

--- слушаем сетевое  соединение/клиент/go/src/main.go
+++ слушаем сетевое  соединение/клиент/go/src/main.go
@@ -12,20 +12,27 @@ func main() {
 	// Подключаемся к сокету
 	conn, err := net.Dial("tcp", "127.0.0.1:8080")
 	if err != nil {
-		panic(err)
+		fmt.Print("not connection server")
 	}
 	for {
 		// Чтение входных данных от stdin
-		reader := bufio.NewReader(os.Stdin)
-		fmt.Print("Text to send: ")
-		text, err := reader.ReadString('\n')
+		reader := bufio.NewReader(os.Stdin) // ждём  ввода
+		fmt.Print("enter: ")
+		text, err := reader.ReadString('\n')// считываем ввод
 		if err != nil {
 			panic(err)
+		}
+		if text == "Exit" {
+			os.Exit(1)
 		}
 		// Отправляем в socket
 		fmt.Fprintf(conn, text+"\n")
 		// Прослушиваем ответ
-		message, _ := bufio.NewReader(conn).ReadString('\n')
+		message, err := bufio.NewReader(conn).ReadString('\n')
+		if err != nil {
+			fmt.Print("not server " + message)
+			panic(err)
+		}
 		fmt.Print("not server " + message)
 	}
 }
